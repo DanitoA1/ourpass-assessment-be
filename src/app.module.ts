2c9089cab3c36604b3d372433afd061279243506
DanitoA1 <ahiabadaniel@gmail.com>
2023-03-15 14:30:06 +0100

    update app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,14 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { User } from './typeorm/entities/User';
+import { UsersModule } from './users/users.module';
+import { JwtModule, JwtService } from '@nestjs/jwt';
+import { User } from './users/entities/user.entity';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: 'localhost',
@@ -16,8 +20,10 @@ import { User } from './typeorm/entities/User';
       entities: [User],
       synchronize: true,
     }),
+    UsersModule,
+    TypeOrmModule.forFeature([User]),
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, JwtService, ConfigService],
 })
 export class AppModule {}
