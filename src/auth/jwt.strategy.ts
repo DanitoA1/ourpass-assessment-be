cecd0424598719b57ddced125485f3ce879d5f6f
DanitoA1 <ahiabadaniel@gmail.com>
2023-03-16 11:34:35 +0100

    update user auth-guard strategy

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -2,10 +2,14 @@ import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { UsersService } from 'src/users/users.service';
 
 @Injectable()
 export class UserStrategy extends PassportStrategy(Strategy, 'user') {
-  constructor(configService: ConfigService) {
+  constructor(
+    configService: ConfigService,
+    private usersService: UsersService,
+  ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
@@ -13,5 +17,8 @@ export class UserStrategy extends PassportStrategy(Strategy, 'user') {
     });
   }
 
-  async validate(payload: any) {}
+  async validate(payload: any) {
+    const user = await this.usersService.getUserByEmail(payload.email);
+    return user;
+  }
 }
