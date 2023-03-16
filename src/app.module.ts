52043f117153aceb0fae77225c016eb14030fc14
DanitoA1 <ahiabadaniel@gmail.com>
2023-03-16 11:34:05 +0100

    update base route welcome message

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,12 @@ import { UsersModule } from './users/users.module';
 import { JwtModule, JwtService } from '@nestjs/jwt';
 import { User } from './users/entities/user.entity';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TokenModule } from './token/token.module';
+import { UserToken } from './token/entities/token.entity';
+import { UserStrategy } from './auth/jwt.strategy';
+import { UsersService } from './users/users.service';
+import { TokenService } from './token/token.service';
+import { CategoriesModule } from './categories/categories.module';
 
 @Module({
   imports: [
@@ -17,13 +23,22 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
       username: 'root',
       password: '',
       database: 'ourpass_assessment',
-      entities: [User],
+      entities: [User, UserToken],
       synchronize: true,
     }),
     UsersModule,
-    TypeOrmModule.forFeature([User]),
+    TypeOrmModule.forFeature([User, UserToken]),
+    TokenModule,
+    CategoriesModule,
   ],
   controllers: [AppController],
-  providers: [AppService, JwtService, ConfigService],
+  providers: [
+    AppService,
+    JwtService,
+    ConfigService,
+    UserStrategy,
+    UsersService,
+    TokenService,
+  ],
 })
 export class AppModule {}
