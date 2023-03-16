b0b71e2b05f6d14142bdfe79e61f0098e81eb609
DanitoA1 <ahiabadaniel@gmail.com>
2023-03-16 19:26:04 +0100

    disallow use of older token after new generation

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -1,24 +1,30 @@
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { UsersService } from 'src/users/users.service';
+import { TokenService } from 'src/token/token.service';
 
 @Injectable()
 export class UserStrategy extends PassportStrategy(Strategy, 'user') {
   constructor(
     configService: ConfigService,
     private usersService: UsersService,
+    private tokenService: TokenService,
   ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
       secretOrKey: configService.get<string>('JWT_SECRET_KEY'),
+      passReqToCallback: true,
     });
   }
 
-  async validate(payload: any) {
+  async validate(req: Request, payload: any) {
     const user = await this.usersService.getUserByEmail(payload.email);
+    const token = ExtractJwt.fromAuthHeaderAsBearerToken()(req);
+    const storedToken = await this.tokenService.getToken(token);
+    if (!storedToken) throw new UnauthorizedException('Token no longer valid');
     return user;
   }
 }
